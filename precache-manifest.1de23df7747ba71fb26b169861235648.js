fb26b1b0f53346f91e18e7e1ffea38335f442dc9
locdb <locdb@vng.com.vn>
2018-11-24 00:21:30 +0700

    fix rorate issue

---
+++ precache-manifest.1de23df7747ba71fb26b169861235648.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f845aadcc72ecfa3eaf",
+    "url": "/static/js/main.0f845aad.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "0f845aadcc72ecfa3eaf",
+    "url": "/static/css/main.e89be5f3.chunk.css"
+  },
+  {
+    "revision": "46eca67a818539b4a419d232b3d6f603",
+    "url": "/index.html"
+  }
+];
